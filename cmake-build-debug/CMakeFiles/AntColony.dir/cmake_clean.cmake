801bcc90e77f97ab19c1a48280f6de7e2ce11c65
Federico Masiero <federico.masiero.1@studenti.unipd.it>
2019-10-15 12:16:00 +0200

    First Commit

---
+++ cmake-build-debug/CMakeFiles/AntColony.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AntColony.exe"
+  "AntColony.exe.manifest"
+  "AntColony.lib"
+  "AntColony.pdb"
+  "CMakeFiles/AntColony.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AntColony.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
